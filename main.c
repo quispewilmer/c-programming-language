da44ee30ae1df8478692f97fd9acab455daec8a0
quispewilmer <wilmerquispecode@gmail.com>
2022-12-16 19:40:43 -0500

    Add math with pointers

--- main.c
+++ main.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
+#include "math.h"
 
 int main() {
-    printf("Hello world!");
+    int base = 2, exp = 5;
+
+    printf("Hello world!\n");
+    printf("Exponential result is: %d\n", power(&base, &exp));
 
     return 0;
 }
